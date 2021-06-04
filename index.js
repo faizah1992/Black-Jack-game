276289772deba3b1a8f2d313107a2c3d6a9330ec
Faizah Ahsan <faizah1992@gmail.com>
2021-06-04 10:08:07 -0500

    some logic added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let firstCard = 10
 let secondCard = 7
 let sum = firstCard + secondCard + 6 // 23
+let hasBlackJack = false
 
-if (sum < 21) {
+if (sum <= 20) {
     console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
     console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
 } else {
     console.log("You're out of the game! 😭")
 }
