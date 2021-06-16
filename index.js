8f09f574e621d19fb76e7f304a705ecad2e38ae8
Faizah Ahsan <faizah1992@gmail.com>
2021-06-16 00:05:48 -0500

    new function added

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    return 5
+}
+
+
+function startGame() {
+    renderGame()
+}
 
 function renderGame() {
     cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -27,14 +37,11 @@ function renderGame() {
     messageEl.textContent = message
 }
 
-function startGame(){
-    renderGame()
-}
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-    let card = 7
+    let card = getRandomCard()
     sum += card
     cards.push(card)
+    console.log(cards)
     renderGame()
 }
